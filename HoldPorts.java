a30c8121edf5be311e7bc9c62a0621888c241cf1
Dale Emery <demery@vmware.com>
2021-05-19 11:08:54 -0700

    Forbid reuse of port

--- HoldPorts.java
+++ HoldPorts.java
@@ -3,14 +3,12 @@ import static java.util.stream.Collectors.toSet;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
-import java.net.SocketTimeoutException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
@@ -25,9 +23,18 @@ public class HoldPorts implements Runnable {
     try {
       ALL_ADDRESSES = Collections.list(NetworkInterface.getNetworkInterfaces()).stream()
         .flatMap(NetworkInterface::inetAddresses)
+        .filter(HoldPorts::isReachable)
         .collect(toSet());
     } catch (SocketException e) {
       throw new UncheckedIOException(e);
+    }
+  }
+
+  private static boolean isReachable(InetAddress addr) {
+    try {
+      return addr.isReachable(100);
+    } catch (IOException e) {
+      return false;
     }
   }
 
@@ -62,15 +69,36 @@ public class HoldPorts implements Runnable {
   public void run() {
     try (ServerSocket server = bind(addr)) {
       try (Socket client = connect(server)) {
-        server.accept();
-        System.out.printf("Holding %d on %s%n", addr.getPort(), addr.getAddress());
-        while (true) {
-          Thread.sleep(120_000);
+        try {
+          server.accept();
+        } catch (IOException e) {
+          System.out.printf("Cannot accept %s:%d (client %s:%d): %s%n",
+            server.getInetAddress(), server.getLocalPort(),
+            client.getLocalAddress(), client.getLocalPort(),
+            e
+          );
+          return;
         }
+        System.out.printf("Holding %s:%d%n",
+          server.getInetAddress(), server.getLocalPort()
+          );
+        while (true) {
+          try {
+            Thread.sleep(60_000);
+          } catch (InterruptedException e) {
+          }
+        }
+      } catch (IOException e) {
+        System.out.printf("Cannot connect to %s:%d: %s%n",
+          server.getInetAddress(), server.getLocalPort(),
+          e
+        );
       }
-    } catch (InterruptedException | SocketTimeoutException | ConnectException e) {
     } catch (IOException e) {
-      System.out.printf("Could not hold %s: %s%n", addr, e);
+      System.out.printf("Cannot bind %s:%d: %s%n",
+        addr.getAddress(), addr.getPort(),
+        e
+      );
     }
   }
 
@@ -83,7 +111,7 @@ public class HoldPorts implements Runnable {
   private static ServerSocket bind(InetSocketAddress addr) throws IOException {
     ServerSocket server = new ServerSocket();
     server.setReuseAddress(true);
-    server.setOption(SO_REUSEPORT, true);
+    server.setOption(SO_REUSEPORT, false);
     server.setSoTimeout(120_000);
     server.bind(addr);
     return server;
